bef172c348c8884a6af59fa94a34ce5c2bc4801f
Oğuzhan Tekin <119113636+tekinoguzhan@users.noreply.github.com>
2023-08-12 17:04:41 +0300

    Add files via upload

---
+++ src/test/java/com/otekin/dev/demo/springdatajdbcazurepostgrescrud/SpringDataJdbcAzurePostgresCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otekin.dev.demo.springdatajdbcazurepostgrescrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJdbcAzurePostgresCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
